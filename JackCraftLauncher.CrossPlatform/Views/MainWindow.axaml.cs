e9006db5f9db608fdae582c744f8c3bddb5d311d
imJack6 <3486688394@qq.com>
2023-05-20 01:55:03 +0800

    基础UI

--- JackCraftLauncher.CrossPlatform/Views/MainWindow.axaml.cs
+++ JackCraftLauncher.CrossPlatform/Views/MainWindow.axaml.cs
@@ -1,6 +1,6 @@
 using Avalonia.Controls;
 
-namespace JackCraftLauncher.CrossPlatform;
+namespace JackCraftLauncher.CrossPlatform.Views;
 
 public partial class MainWindow : Window
 {
---
+++ JackCraftLauncher.CrossPlatform/Views/MyControls/AnimateTabControl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
+using Avalonia.Styling;
+
+namespace JackCraftLauncher.CrossPlatform.Views.MyControls
+{
+    [PseudoClasses(":normal")]
+    public class AnimateTabControl : TabControl, IStyleable
+    {
+        Type IStyleable.StyleKey => typeof(TabControl);
+
+        public AnimateTabControl()
+        {
+            PseudoClasses.Add(":normal");
+            this.GetObservable(SelectedContentProperty).Subscribe(OnContentChanged);
+        }
+
+        private void OnContentChanged(object obj)
+        {
+            if (AnimateOnChange)
+            {
+                PseudoClasses.Remove(":normal");
+                PseudoClasses.Add(":normal");
+            }
+        }
+
+        public bool AnimateOnChange
+        {
+            get => GetValue(AnimateOnChangeProperty);
+            set => SetValue(AnimateOnChangeProperty, value);
+        }
+
+        public static readonly StyledProperty<bool> AnimateOnChangeProperty =
+            AvaloniaProperty.Register<AnimateTabControl, bool>(nameof(AnimateOnChange), true);
+    }
+}
