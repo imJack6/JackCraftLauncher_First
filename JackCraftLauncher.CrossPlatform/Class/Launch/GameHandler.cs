08585f4ea56d1655b28c120450524794de755dd9
imJack6 <3486688394@qq.com>
2023-05-20 16:34:55 +0800

    刷新本地游戏列表

---
+++ JackCraftLauncher.CrossPlatform/Class/Launch/GameHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using JackCraftLauncher.CrossPlatform.Views.Menu;
+
+namespace JackCraftLauncher.CrossPlatform.Class.Launch;
+
+public class GameHandler
+{
+    public static void RefreshLocalGameList()
+    {
+        GlobalVariable.LocalGameList = GlobalVariable.LaunchCore.GetCore().VersionLocator.GetAllGames().ToList();
+        StartUserControl.Instance.SelectJavaGameVersionComboBox.ItemsSource = GlobalVariable.LocalGameList.Select(x => x.Name ?? x.Id);
+    }
+}
