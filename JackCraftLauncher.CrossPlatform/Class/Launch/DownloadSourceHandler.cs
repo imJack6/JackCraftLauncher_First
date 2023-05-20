cf5609697f187ea7923eb95d090d7f78611c5c92
imJack6 <3486688394@qq.com>
2023-05-20 22:54:06 +0800

    优化代码

--- JackCraftLauncher.CrossPlatform/Class/Launch/DownloadSourceHandler.cs
+++ JackCraftLauncher.CrossPlatform/Class/Launch/DownloadSourceHandler.cs
@@ -43,8 +43,35 @@ public class DownloadSourceHandler
                     break;
             }
         }
-        var baseUrl = "";
-        switch (source)
+        var baseUrl = source switch
+        {
+            DownloadSourceEnum.MCBBS => "https://download.mcbbs.net",
+            DownloadSourceEnum.BMCL => "https://bmclapi2.bangbang93.com",
+            DownloadSourceEnum.Official => target switch
+            {
+                DownloadTargetEnum.MinecraftJar or DownloadTargetEnum.MinecraftJson => "https://download.mcbbs.net",
+                DownloadTargetEnum.MinecraftLibraries => "https://libraries.minecraft.net",
+                DownloadTargetEnum.MinecraftAssets => "http://resources.download.minecraft.net",
+                DownloadTargetEnum.MinecraftAssetsIndex => "https://launchermeta.mojang.com",
+                _ => "http://launchermeta.mojang.com"
+            },
+            _ => throw new InvalidDataException($"Selected mirror field {source} does not exist.")
+        };
+        return target switch
+        {
+            DownloadTargetEnum.VersionInfoV1 => $"{baseUrl}/mc/game/version_manifest.json",
+            DownloadTargetEnum.VersionInfoV2 => $"{baseUrl}/mc/game/version_manifest_v2.json",
+            DownloadTargetEnum.MinecraftJar => $"{baseUrl}/version/{minecraftVersion}/client",
+            DownloadTargetEnum.MinecraftJson => $"{baseUrl}/version/{minecraftVersion}/json",
+            DownloadTargetEnum.MinecraftLibraries when source == DownloadSourceEnum.Official => $"{baseUrl}/",
+            DownloadTargetEnum.MinecraftLibraries => $"{baseUrl}/maven/",
+            DownloadTargetEnum.MinecraftAssets when source == DownloadSourceEnum.Official => $"{baseUrl}/",
+            DownloadTargetEnum.MinecraftAssets => $"{baseUrl}/assets/",
+            DownloadTargetEnum.MinecraftAssetsIndex => $"{baseUrl}/",
+            _ => throw new InvalidDataException($"Selected target field {target} does not exist.")
+        };
+        #region Old
+        /*switch (source)
         {
             case DownloadSourceEnum.BMCL:
                 baseUrl = "https://bmclapi2.bangbang93.com";
@@ -92,7 +119,8 @@ public class DownloadSourceHandler
                 return $"{baseUrl}/";
             default:
                 throw new InvalidDataException($"Selected mirror field {source} does not found");
-        }
+        }*/
+        #endregion
     }
 
     public static string PistonMetaUrlHandle(DownloadSourceEnum source, string url)
