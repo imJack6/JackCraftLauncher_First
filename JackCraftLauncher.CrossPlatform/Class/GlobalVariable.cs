685e0e318660e5761b8b3f102a841f0533dc0e45
imJack6 <3486688394@qq.com>
2023-05-20 16:34:44 +0800

    增加全局变量

---
+++ JackCraftLauncher.CrossPlatform/Class/GlobalVariable.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using JackCraftLauncher.CrossPlatform.Class.Launch;
+using ProjBobcat.Class.Model;
+
+namespace JackCraftLauncher.CrossPlatform.Class;
+
+public static class GlobalVariable
+{
+    public static LaunchCoreHandler LaunchCore = new LaunchCoreHandler();
+    public static List<VersionInfo> LocalGameList = new List<VersionInfo>();
+}
