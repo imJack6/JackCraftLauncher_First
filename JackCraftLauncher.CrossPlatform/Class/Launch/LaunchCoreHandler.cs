a0a88c6efe5129e69bf5ce12317553d179e6a16b
imJack6 <3486688394@qq.com>
2023-05-20 16:35:06 +0800

    初始化启动器核心

---
+++ JackCraftLauncher.CrossPlatform/Class/Launch/LaunchCoreHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using ProjBobcat.DefaultComponent.Launch;
+using ProjBobcat.DefaultComponent.Launch.GameCore;
+using ProjBobcat.DefaultComponent.Logging;
+
+namespace JackCraftLauncher.CrossPlatform.Class.Launch;
+
+public class LaunchCoreHandler
+{
+    private DefaultGameCore _core = new DefaultGameCore();
+
+    public LaunchCoreHandler()
+    {
+        Init();
+    }
+    
+    public void Init()
+    {
+        Guid clientToken = new Guid("11451419-1981-0114-5141-919810114514");
+        string rootPath = "JCL/.minecraft/";
+        _core = new DefaultGameCore
+        {
+            ClientToken = clientToken,
+            RootPath = rootPath,
+            VersionLocator = new DefaultVersionLocator(rootPath, clientToken)
+            {
+                LauncherProfileParser = new DefaultLauncherProfileParser(rootPath, clientToken),
+                LauncherAccountParser = new DefaultLauncherAccountParser(rootPath, clientToken)
+            },
+            GameLogResolver = new DefaultGameLogResolver()
+        };
+    }
+    public DefaultGameCore GetCore()
+    {
+        return _core;
+    }
+}
