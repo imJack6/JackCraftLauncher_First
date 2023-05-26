b70512d0fa0b7a024006b03aef0ec666012a7dae
imJack6 <3486688394@qq.com>
2023-05-26 19:27:14 +0800

    测试Windows版启动

--- JackCraftLauncher.CrossPlatform/Views/Menu/StartUserControl.axaml.cs
+++ JackCraftLauncher.CrossPlatform/Views/Menu/StartUserControl.axaml.cs
@@ -3,6 +3,8 @@ using Avalonia.Interactivity;
 using JackCraftLauncher.CrossPlatform.Class;
 using JackCraftLauncher.CrossPlatform.Class.Launch;
 using JackCraftLauncher.CrossPlatform.Views.MyControls;
+using ProjBobcat.Class.Model;
+using ProjBobcat.DefaultComponent.Launch.GameCore;
 
 namespace JackCraftLauncher.CrossPlatform.Views.Menu;
 
@@ -26,9 +28,20 @@ public partial class StartUserControl : UserControl
         else
             GameHandler.StartGame(GlobalVariable.LocalGameList[SelectJavaGameVersionComboBox.SelectedIndex]);
     }
+    
+    private void StartBedrockGameButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var launchsettings = new LaunchSettings()
+        {
+            
+        };
+        DefaultMineCraftUWPCore core = new DefaultMineCraftUWPCore();
+        core.Launch(launchsettings);
+    }
 
     private void RefreshLocalGameComboListButton_OnClick(object? sender, RoutedEventArgs e)
     {
         GameHandler.RefreshLocalGameList();
     }
+
 }
