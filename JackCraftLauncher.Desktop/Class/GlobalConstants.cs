721e348f34e95fb121f49ea4b7bcd859fc9ba899
imJack6 <3486688394@qq.com>
2023-05-28 01:37:09 +0800

    全局常量

---
+++ JackCraftLauncher.Desktop/Class/GlobalConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace JackCraftLauncher.Desktop.Class;
+
+public class GlobalConstants
+{
+    public const string ConfigThemeNode = "LauncherSettings.Theme";
+    public const string ConfigDownloadSourceNode = "DownloadSettings.DownloadSource";
+    
+}
