9e1a602db791611f3f2b6c769fcae0e813b30c23
imJack6 <3486688394@qq.com>
2023-06-02 23:40:22 +0800

    插件功能仅存储

---
+++ JackCraftLauncher.Desktop/Interface/IPlugin.cs
@@ -0,0 +1,10 @@
+﻿namespace JackCraftLauncher.Desktop.Interface;
+
+public interface IPlugin
+{
+    string Name { get; }
+    string Description { get; }
+    string Author { get; }
+    string Version { get; }
+    void Execute();
+}
